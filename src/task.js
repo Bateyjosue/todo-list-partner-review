195f89997ea056f48de1cee2c48d64686dbfa848
Bateyjosue <josuebatey19@gmail.com>
2022-08-12 10:00:30 +0200

    Add Task collection

---
+++ src/task.js
@@ -0,0 +1,25 @@
+export default class Task {
+  constructor(description) {
+    this.tasks = JSON.parse(localStorage.getItem('toDoList')) || [];
+    this.index = null;
+    this.completed = false;
+    this.description = description;
+  }
+
+  addTask = (description) => {
+    this.tasks.push({
+      index: this.tasks.length,
+      description,
+      completed: false,
+    });
+  };
+
+  removerTask = (index) => {
+    this.tasks.splice(index, 1);
+    this.tasks = this.tasks.map((task, index) => ({
+      ...task, index,
+    }));
+  };
+
+  updateTask = () => localStorage.setItem('toDoList', JSON.stringify(this.tasks));
+}
